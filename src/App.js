993d26d7c6d7f8dcd2308886ee34874585ac3ecf
markussite <59604552+markussite@users.noreply.github.com>
2023-01-14 20:20:10 +0100

    added tracks and artist

--- src/App.js
+++ src/App.js
@@ -12,21 +12,6 @@ function App() {
   const [accessToken, setAccessToken] = useState("");
   const [albums, setAlbum] = useState([]);
 
-  useEffect(() => {
-    //API Acces token
-
-    const authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded' //spotify wants it specifly like that
-      },
-      body: 'grant_type=client_credentials&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET
-    }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(result => result.json())
-      .then(data => setAccessToken(data.access_token)) //errorHandling for fetch
-  }, []) //setting api code
-
   async function search() {
     console.log("Search for " + searchInput);
 
